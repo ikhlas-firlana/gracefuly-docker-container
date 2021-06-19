86c6a2dc6e4aec9a89a881fc5d166826a5da1971
ikhlas-firlana <ifirlana@gmail.com>
2021-06-19 07:19:24 +0700

    done

---
+++ gracefuly-shutdown.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker kill --signal=SIGINT poc-gracefuly
+
--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 docker build -t poc-gracefuly .
 docker rm poc-gracefuly || true
 docker run -d --name=poc-gracefuly poc-gracefuly:latest
-sleep 10
-docker kill --signal=SIGINT poc-gracefuly
 
